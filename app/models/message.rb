17343d49e61063ed9cfa1526db6b02ae39aa1ac0
kazuyukiyokoyama <dime72.elephant@gmail.com>
2016-02-21 11:04:57 +0000

    age col add and less_than_or_equal_to changed

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,6 @@ class Message < ActiveRecord::Base
   validates :name , length: { maximum: 20 } , presence: true
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
-  #年齢は０歳以上
-  validates :age , numericality: {greater_than_or_equal_to: 0} , presence: true
+  #年齢は0歳以上200歳以下
+  validates :age , numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 200, :only_integer => true} , presence: true
 end
